48d87aa54e30b20e611e17e1609e87ac71c64109
arz <alireza.zarrabi@myboost.co>
2023-03-07 14:54:10 +0800

    removing unnecessary generateDisplayNameForMethod from SimulatorDisplayNameGenerator

--- src/test/java/com/github/mimsic/bdd/sj5/generator/SimulatorDisplayNameGenerator.java
+++ src/test/java/com/github/mimsic/bdd/sj5/generator/SimulatorDisplayNameGenerator.java
@@ -1,10 +1,7 @@
 package com.github.mimsic.bdd.sj5.generator;
 
 import com.github.mimsic.bdd.sj5.annotation.Story;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGenerator;
-
-import java.lang.reflect.Method;
 
 public class SimulatorDisplayNameGenerator extends DisplayNameGenerator.Standard {
 
@@ -16,15 +13,5 @@ public class SimulatorDisplayNameGenerator extends DisplayNameGenerator.Standard
             return storyAnnotation.description();
         }
         return super.generateDisplayNameForClass(testClass);
-    }
-
-    @Override
-    public String generateDisplayNameForMethod(Class<?> testClass, Method testMethod) {
-
-        DisplayName annotation = testMethod.getAnnotation(DisplayName.class);
-        if (annotation != null && annotation.value().length() > 0) {
-            return annotation.value();
-        }
-        return super.generateDisplayNameForMethod(testClass, testMethod);
     }
 }
